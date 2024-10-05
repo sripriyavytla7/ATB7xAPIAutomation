6eb6e9fe80a94c544a275f1351d2cb0828813d8a
sripriyavytla <sri.priya@celigo.com>
2024-10-06 00:06:44 +0530

    added readme file

---
+++ src/test/java/com/testingacademy/Test001.java
@@ -0,0 +1,10 @@
+package com.testingacademy;
+
+import io.restassured.RestAssured;
+
+public class Test001
+{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
